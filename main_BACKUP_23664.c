e1e99685e6b206f1a347ae8d48d170095fee4eb0
Ubuntu <ubuntu@ip-172-31-14-240.us-east-2.compute.internal>
2021-04-09 07:54:28 +0000

    commiting

---
+++ main_BACKUP_23664.c
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+=======
+# Solving the merge conflict issue in git
+>>>>>>> Update and rename main.py to main.c
+pseudo code
+{
+Initial Function()
+{
+  //Sample Code
+}
+
+<<<<<<< HEAD
+Initial Security()
+{
+ //Sample Code
+}
+
+
+
+=======
+feaature2()
+{
+  //Sample Code for Feature 2
+}
+
+
+>>>>>>> Update and rename main.py to main.c
+}
